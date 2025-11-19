60cde40c6762537f945ecb039f966f37aee34d3d
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2025-11-19 21:54:42 +0700

    update pdf view 2

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -33,7 +33,7 @@ class SecurityHeaders
                 "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com; " .
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net; " .
                 "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net data:; " .
-                "img-src 'self' data: https:; " .
+                "img-src 'self' data: blob: https:; " .
                 "connect-src 'self'; " .
                 "frame-src 'self' https://www.google.com https://maps.google.com;"
             );
