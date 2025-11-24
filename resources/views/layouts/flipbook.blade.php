f81d44d05295d885e559a45ca24f4ef4ea7a9d64
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2025-11-24 15:31:56 +0700

    adding buku wisuda slug v5

--- resources/views/layouts/flipbook.blade.php
+++ resources/views/layouts/flipbook.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Buku Wisuda' }}</title>
-    <script defer src="https://cdn.tailwindcss.com"></script>
-    {{ $slot }}
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 <body class="m-0 p-0 overflow-hidden">
     {{ $slot }}
+
+    @livewireScripts
 </body>
 </html>
