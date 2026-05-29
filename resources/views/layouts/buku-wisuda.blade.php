de5ef91593f128ed6c0576b062efeb9a916c0f02
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2026-05-29 16:51:31 +0700

    fix: add Livewire assets to buku-wisuda layout to prevent 500 error

--- resources/views/layouts/buku-wisuda.blade.php
+++ resources/views/layouts/buku-wisuda.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Buku Wisuda' }}</title>
-    @vite
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 <body class="bg-gray-100">
     {{ $slot }}
+    @livewireScripts
 </body>
 </html>
