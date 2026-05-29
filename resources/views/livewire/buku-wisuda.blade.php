117b5c9154e6dd59d0b9b911f151cdf256d59775
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2026-05-29 21:54:37 +0700

    Remove upload date from buku wisuda card, keep file size and downloads

--- resources/views/livewire/buku-wisuda.blade.php
+++ resources/views/livewire/buku-wisuda.blade.php
@@ -103,14 +103,6 @@
                                         </svg>
                                         <span>{{ $buku->download_count }} Download</span>
                                     </div>
-
-                                    <!-- Upload Date -->
-                                    <div class="flex items-center text-sm text-gray-600">
-                                        <svg class="w-4 h-4 mr-2 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                                        </svg>
-                                        <span>{{ $buku->uploaded_at->format('d M Y') }}</span>
-                                    </div>
                                 </div>
                             </div>
 
